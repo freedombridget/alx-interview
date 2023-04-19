9361bf29a07e1992323f8e6ff18cb674d63b1ff7
Bridget Ofejiro Freedom <86961410+freedombridget@users.noreply.github.com>
2023-04-19 23:58:52 +0100

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,42 +5,22 @@ UTF-8 Validation
 """
 
 
-def valid_utf8(data):
-    """
-    Determine if a given data set represents a valid UTF-8 encoding.
-
-    Args:
-        data (list of integers):
-        The data set to be checked for a valid UTF-8 encoding.
-        Each integer represents 1 byte of data, so only the 8 least
-        significant bits of each integer are considered.
-
-Returns:
-bool: True if data is a valid UTF-8 encoding, else False.
-    """
-    byte_count = 0
-
-    for i in data:
-        if byte_count == 0:
-            # If the first few bits of the byte
-            # indicate a 1 to 4 byte character,
-            # set byte_count to the number of bytes minus 1.
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            # If the first bit of the byte is
-            # set to 1, it is a continuation byte
-            # instead of a first byte, so return False.
-            elif i >> 7 == 0b1:
+def validUTF8(data):
+    num_bytes = 0
+    for byte in data:
+        # Check if current byte is a start byte
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
                 return False
         else:
-            # If the byte is not a continuation byte, return False.
-            if i >> 6 != 0b10:
+            # Check if current byte is a continuation byte
+            if (byte >> 6) != 0b10:
                 return False
-            # If this is the last byte of the character, decrement byte_count.
-            byte_count -= 1
-    # If there is no incomplete character at the end, return True.
-    return byte_count == 0
+            num_bytes -= 1
+    return num_bytes == 0
